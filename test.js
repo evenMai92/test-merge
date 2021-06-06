e14488839fe72f2b5e1f59d8ff8a2686cdc14e47
evenMai <410863605@qq.com>
2021-06-06 19:29:58 +0800

    feat: 8888

---
+++ test.js
@@ -0,0 +1,55 @@
+function test() {
+  console.log(123);
+}
+console.log(2233);
+function dddd() {
+  dddd;
+  console.log('ee');
+}
+console.log(fff);
+function dddss() {
+  console.log(223);
+}
+function adf() {
+  console.log(123);
+}
+console.log('dddss');
+1123344
+const aa = 'ffffdd';
+
+const bb = 1233;
+2
+3
+dddd
+1
+dddss
+22221
+function test() {
+  console.log(123);
+}
+console.log(2233);
+function dddd() {
+  dddd;
+  console.log('ee');
+}
+function adf() {
+  console.log(123);
+}
+console.log('dddss');
+1123344
+const aa = 'ffffdd';
+5555
+dddd
+11233
+3
+12
+1111
+333
+222
+cccc
+eee
+1
+ddddddddddddddddd
+222
+44
+ccc
